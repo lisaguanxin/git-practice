f5e0a9d23003e2546ef68e1a7b7d9574a09d5ff9
dy2333 <yangdi@yangdideMacBook-Pro.local>
2016-10-24 15:06:43 -0400

    Add dy2333.js

---
+++ hf-one/dy2333.js
@@ -0,0 +1,21 @@
+var name="Dee";
+var age="23";
+prompt ("Do you want to check Odd/Even Numbers?");
+var n = prompt("Enter a number to find odd or even", "Type your number here");
+n = parseInt(n);
+if (isNaN(n))
+{
+alert("Please Enter a Number");
+}
+else if (n == 0)
+{
+alert("The number is zero");
+}
+else if (n%2)
+{
+alert("The number is odd");
+}
+else
+{
+alert("The number is even");
+}
